06cd2f67704a30399db9541b0843f38477225c92
FLOYD-psm <107343662+SungminPark-FLOYD@users.noreply.github.com>
2023-02-18 16:14:19 +0900

    HUD
    HUD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,14 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     [Header("# Game Control")]
     public float gameTime;
-    public float maxGameTime = 2 * 10f;
+    public float maxGameTime;
     [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
-    public int[] nextExp = {3, 5, 10, 100, 150, 210, 280, 360, 450, 600};
+    public int[] nextExp = {3, 5, 10, 20, 150, 210, 280, 360, 450, 600};
     [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
@@ -21,6 +23,11 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         instance = this; //자기자신으로 초기화
+    }
+
+    void Start()
+    {
+        health = maxHealth;
     }
 
     void Update()
